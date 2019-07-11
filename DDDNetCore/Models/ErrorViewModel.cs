c9d78f95bf9ebe62b03fd369540e80b6057b71ce
GedGonz <gedgonz7@gmail.com>
2019-07-10 19:33:15 -0600

    Agregar archivos de proyecto.

---
+++ DDDNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DDDNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
